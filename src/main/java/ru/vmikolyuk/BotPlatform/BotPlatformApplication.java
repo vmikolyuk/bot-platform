1d9689bb9fb1be5ce551463ada0efb852537f92a
Вячеслав <vmikolyuk@naumen.ru>
2024-05-15 11:02:13 +0300

    Init spring app

---
+++ src/main/java/ru/vmikolyuk/BotPlatform/BotPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.vmikolyuk.BotPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vmikolyuk/BotPlatform/BotPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vmikolyuk.BotPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
